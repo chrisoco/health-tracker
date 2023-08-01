8268bdbc7407db9c600c600ab5db9ae49107bc2a
chrisoco <it.chris.oc@gmail.com>
2023-08-01 11:47:05 +0200

    tmp

--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/example', function () {
+    return Inertia::render('Example/Index');
+});
+
+require __DIR__ . '/auth.php';
