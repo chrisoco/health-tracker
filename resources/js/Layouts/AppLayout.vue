d0937d033acd49c1017ebb6d102cc09be3e2b4e1
chrisoco <it.chris.oc@gmail.com>
2023-08-01 21:58:47 +0200

    main Layout

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -13,12 +13,14 @@ const showingNavigationDropdown = ref(false);
 <template>
     <div class="min-h-screen bg-gray-100">
         <div class="mx-2 lg:mx-0">
-            <div class="drawer lg:drawer-open">
+
+
+            <div class="drawer">
                 <input id="my-drawer-3" type="checkbox" class="drawer-toggle" /> 
                 <div class="drawer-content flex flex-col">
-                    <!-- Navbar -->
-                    <div class="w-full navbar bg-base-100 rounded-xl lg:rounded-none mt-1 lg:mt-0 mb-1">
-                        <div class="flex-none lg:hidden">
+                    <!-- Mobile Navbar -->
+                    <div class="lg:hidden w-full navbar bg-base-100 rounded-xl mt-1 mb-1">
+                        <div class="flex-none">
                             <label for="my-drawer-3" class="btn btn-square btn-ghost">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                             </label>
@@ -59,15 +61,72 @@ const showingNavigationDropdown = ref(false);
                         </div>
                     </div>
 
-                    <!-- Page content here -->
-                    
-                    Content
-                    <div class="sm:hidden">Mobile</div>
-                    <div class="sm:visible md:hidden">SM</div>
-                    <div class="md:visible lg:hidden">MD</div>
-                    <div class="lg:visible xl:hidden">LG</div>
+                    <!-- PC Navbar -->
+                    <nav class="hidden lg:block bg-white border-b border-gray-100">
+                        <!-- Primary Navigation Menu -->
+                        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                            <div class="flex justify-between h-16">
+                                <div class="flex">
+                                    <!-- Logo -->
+                                    <div class="shrink-0 flex items-center">
+                                        <Link :href="route('dashboard')">
+                                            <ApplicationLogo
+                                                class="block h-9 w-auto fill-current text-gray-800"
+                                            />
+                                        </Link>
+                                    </div>
+
+                                    <!-- Navigation Links -->
+                                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                                        <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                                            Dashboard
+                                        </NavLink>
+                                        <NavLink :href="route('example')" :active="route().current('example')">
+                                            Example
+                                        </NavLink>
+                                    </div>
+                                </div>
+
+                                <div class="hidden sm:flex sm:items-center sm:ml-6">
+                                    <!-- Settings Dropdown -->
+                                    <div class="ml-3 relative">
+                                        <Dropdown align="right" width="48">
+                                            <template #trigger>
+                                                <span class="inline-flex rounded-md">
+                                                    <button type="button" class="inline-flex items-center px-0 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                                        {{ $page.props.auth.user.email }}
+
+                                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                                            <path
+                                                                fill-rule="evenodd"
+                                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                                clip-rule="evenodd"
+                                                            />
+                                                        </svg>
+                                                    </button>
+                                                </span>
+                                            </template>
+
+                                            <template #content>
+                                                <DropdownLink :href="route('profile.edit')"> Profile </DropdownLink>
+                                                <DropdownLink :href="route('logout')" method="post" as="button">
+                                                    Log Out
+                                                </DropdownLink>
+                                            </template>
+                                        </Dropdown>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </nav>
+
+                    <!-- Page Content -->
+                    <main>
+                        <slot />
+                    </main>
+
                 </div> 
-                <div class="drawer-side">
+                <div class="lg:hidden drawer-side">
                     <label for="my-drawer-3" class="drawer-overlay"></label> 
                     <ul class="menu p-4 w-80 h-full" style="background-color:#8031f6;">
                         <ApplicationLogo
