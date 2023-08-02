16eaf5c16367885e5c2efaa2b0df82a5bebf1a99
chrisoco <it.chris.oc@gmail.com>
2023-08-03 00:11:19 +0200

    tmp

--- resources/js/Pages/Models/Smoke/Index.vue
+++ resources/js/Pages/Models/Smoke/Index.vue
@@ -1,7 +1,8 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import { Head } from '@inertiajs/vue3';
 import Widget from '@/Components/Widget.vue';
+import moment from "moment";
+import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
@@ -11,6 +12,10 @@ import Widget from '@/Components/Widget.vue';
         <template #header>
             Example
         </template>
+
+        <Widget>
+          <button class="btn btn-primary">New Smoke</button>
+        </Widget>
 
         <Widget :class="'lg:col-span-2'">
           <table class="table table-zebra text-center">
@@ -26,28 +31,7 @@ import Widget from '@/Components/Widget.vue';
               <!-- row 1 -->
               <tr v-for="entry in today" class="hover">
                 <th>1</th>
-                <td>{{ new Date(entry.created_at).toLocaleTimeString() }}</td>
-                <th><button class="btn btn-xs btn-outline btn-error">Delete</button></th>
-              </tr>
-            </tbody>
-          </table>
-        </Widget>
-
-        <Widget>
-          <table class="table table-zebra text-center">
-            <!-- head -->
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Time</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <!-- row 1 -->
-              <tr v-for="entry in today" class="hover">
-                <th>1</th>
-                <td>{{ new Date(entry.created_at).toLocaleTimeString() }}</td>
+                <td>{{ moment(entry.created_at).format('HH:mm') }}</td>
                 <th><button class="btn btn-xs btn-outline btn-error">Delete</button></th>
               </tr>
             </tbody>
